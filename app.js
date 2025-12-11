c9ce2208d6ed30c66a2a1faf42e50e844e77b410
micky mack <jijomonjs@gmail.com>
2025-12-11 17:00:21 +0530

    upda

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ app.use("/", userRoutes);
 const adminRoutes = require("./routes/adminRoutes");
 app.use("/admin", adminRoutes);
 
+const apiRoutes = require("./routes/apiRoutes");
+app.use("/api", apiRoutes); 
+
 
 app.use((req, res, next) => {
   res.status(404);
